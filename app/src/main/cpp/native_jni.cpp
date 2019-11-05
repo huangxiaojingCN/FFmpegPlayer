800fd4215c11298625a731e632aae5959a5ca732
huangxiaojing <huangxiaojingcn@gmail.com>
2019-11-05 14:00:13 +0800

    Feat: 添加 ffmpeg 静态库，打印版本号.

--- app/src/main/cpp/native_jni.cpp
+++ app/src/main/cpp/native_jni.cpp
@@ -1,9 +1,14 @@
 #include <jni.h>
 
+extern "C" {
+#include "libavcodec/avcodec.h"
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_hxj_ffmpegplayer_MainActivity_messageFromJNI(JNIEnv *env,
         jobject instance) {
-    return env->NewStringUTF("Hello Java. I com from JNI.");
+
+    return env->NewStringUTF(av_version_info());
 }
 
